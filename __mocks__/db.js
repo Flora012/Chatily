580e24928fe93574ab395628c2a7ac872621bc42
Szabó Flóra <szabo.flora@ckik.loc>
2025-02-07 08:44:59 +0100

    Igazabol okes van .env es a server is javitva van

--- __mocks__/db.js
+++ __mocks__/db.js
@@ -1,16 +1,7 @@
 const { Sequelize, DataTypes } = require("sequelize");
 require("dotenv").config();
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host: process.env.DB_HOST,
-        dialect: process.env.DB_DIALECT,
-        logging: false,
-    }
-);
+const sequelize = new Sequelize("sqlite::memory:");
 
 const db = {};
 
--- tests/friendshipServiceTest.test.js
+++ tests/friendshipServiceTest.test.js
@@ -1,16 +1,18 @@
-jest.mock("../api/db/dbContext", () => require("../__mocks__/db"));
+require("dotenv").config();
 
 const friendshipService = require("../api/services/friendshipService");
 const friendshipRepository = require("../api/repositories/friendshipRepository");
 
+jest.mock("../api/db/dbContext", () => require("../__mocks__/db"));
+
 describe("FriendshipService", () => {
-    beforeAll( async () => 
-        {
-            await require("../__mocks__/db").sequelize.sync({ force: true });
-        });
+    beforeAll(async () => {
+        await require("../__mocks__/db").sequelize.sync({ force: true });
+    });
+
     test("should add a friend", async () => {
         const friendshipData = { user_id: 1, friend_id: 2 };
-        friendshipRepository.addFriend.mockResolvedValue(friendshipData);
+        friendshipRepository.addFriend = jest.fn().mockResolvedValue(friendshipData);
 
         const result = await friendshipService.addFriend(friendshipData);
         expect(result).toEqual(friendshipData);
@@ -19,7 +21,7 @@ describe("FriendshipService", () => {
 
     test("should get friends", async () => {
         const friends = [{ id: 2, name: "Friend" }];
-        friendshipRepository.getFriends.mockResolvedValue(friends);
+        friendshipRepository.getFriends = jest.fn().mockResolvedValue(friends);
 
         const result = await friendshipService.getFriends(1);
         expect(result).toEqual(friends);
